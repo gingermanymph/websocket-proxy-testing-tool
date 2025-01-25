9cdd681b23ec500146d6add249f6fd90322c6a32
gingermanymph <gingermanymph@gmail.com>
2025-01-25 21:15:39 +0200

    added helper component

---
+++ websocket-proxy-chrome/src/components/Helpers/ErrorBoundary.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { reloadInspectedWindow, storeManager } from '../../utils/chromeAPI';
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false, error: null };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true, error };
+    }
+
+    componentDidCatch(error, info) {
+        console.error("Error caught in ErrorBoundary:", error, info);
+    }
+
+    panel() {
+        return <div className="debugger">
+            <button onClick={() => reloadInspectedWindow()}>Page reload</button>
+            <button onClick={() => window.location.reload()}>Frame reload</button>
+            <button onClick={() => storeManager.clear()}>Clear storage</button>
+        </div>
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <>
+                {this.panel()}
+                <pre>OOPS: {this.state.error?.message + '\n' + this.state.error?.stack}</pre>
+            </>
+        }
+        return <>{this.panel()} {this.props.children}</>
+    }
+}
+
+export default ErrorBoundary;
