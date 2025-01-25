9adaf555c4d4e87459f87219a972d03eabc9a470
gingermanymph <gingermanymph@gmail.com>
2025-01-25 21:17:26 +0200

    added template strings

---
+++ websocket-proxy-chrome/src/utils/templates.ts
@@ -0,0 +1,23 @@
+export const defaultWspTemplate = `/** This code will runs each time before sending websocket message 
+*  In this code context available two variables \`message\` and \`target\`
+*  @target - contains current WebSocket host
+*  @message - contains current data
+*/
+
+// Skipp host
+if (target === 'wss://example.com/') return message;
+
+// Modify message
+message = message.toUpperCase();\n`;
+
+export const defaultJSLiveTemplate = `message = {
+    // Create your message
+    currentTime: new Date().getTime(),
+    userAgent: navigator.userAgent
+}\n`;
+
+
+export const defaultJSONTemplate = `{
+"message":"Hello WebSockets"
+}`
+
