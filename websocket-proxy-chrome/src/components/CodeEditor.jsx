3c6715ad716495edfae0453c98f867e991b1b138
gingermanymph <gingermanymph@gmail.com>
2025-01-26 23:18:17 +0200

    Added beautifyJSON

--- websocket-proxy-chrome/src/components/CodeEditor.jsx
+++ websocket-proxy-chrome/src/components/CodeEditor.jsx
@@ -4,10 +4,18 @@ import { javascript } from '@codemirror/lang-javascript';
 import { json } from '@codemirror/lang-json';
 
 export function CodeEditor({ value, onChange, extensions, editable = true }) {
+  const beautifyJSON = (jsonString) => {
+    try {
+      return JSON.stringify(JSON.parse(jsonString), null, 2) + '\n\n\n';
+    } catch (e) {
+      return jsonString + '\n\n\n';
+    }
+  }
+
   return (
     <CodeMirror
       height='100%'
-      value={value}
+      value={editable ? value : beautifyJSON(value)}
       extensions={extensions === 'javascript' ? [javascript({ jsx: true })] : [json()]}
       editable={editable}
       onChange={onChange}
