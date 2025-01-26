4c02662347be2de9322506aa806d35cc6369ed99
gingermanymph <gingermanymph@gmail.com>
2025-01-26 21:16:00 +0200

    Fixed min-w/h

--- websocket-proxy-chrome/src/components/Menu.jsx
+++ websocket-proxy-chrome/src/components/Menu.jsx
@@ -20,12 +20,13 @@ const Menu = ({ onSelect, active }) => {
     const [isSelected, setActiveComponent] = useState(active);
     return (
         <div className='flex flex-col w-[30px] border-r border-light'>
-            <div className='flex justify-center items-center h-[26px] border-b border-light '>
+            <div className='flex justify-center items-center min-h-[26px] border-b border-light '>
                 <VscMenu className='h-[18px] w-[18px] text-light'/>
             </div>
             {menuItems.map((item, id) => (
                 <button
-                    className={`w-[30px] h-[30px] flex justify-center items-center text-light ${isSelected === item.type && 'selected' }`}
+                // w-[30px] h-[30px]
+                    className={`min-w-[30px] min-h-[30px] flex justify-center items-center text-light ${isSelected === item.type && 'selected' }`}
                     key={id}
                     onClick={() => {
                         onSelect(item.type);
